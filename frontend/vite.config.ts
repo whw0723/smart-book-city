27e1c7c778b9c77357d364b8e00a397f0971e2c7
whw0723 <774633753@qq.com>
2025-12-18 16:05:25 +0800

    1
    1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/smart-book-city/', // GitHub Pages需要的base路径
   plugins: [vue()],
   resolve: {
     alias: {
